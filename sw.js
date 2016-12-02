32b5a4c719fc3c1e1570efa3623fbe147fbfe63e
Paul Kinlan <paul.kinlan@gmail.com>
2016-12-02 09:27:53 +0000

    PWA ify

---
+++ sw.js
@@ -0,0 +1,36 @@
+/*
+ *
+ *  Device Framers
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('framer').then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/css/main.css',
+        '/scripts/ffmpeg.js',
+        '/scripts/ffmpeg-worker-mp4.js',
+        '/assets/n6-background.png',
+        '/assets/n6-background.mp4'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
